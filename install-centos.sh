22a08f23be173a0e885f04ce8195e9b32ee7d124
tempnana <94152736+tempnana@users.noreply.github.com>
2023-01-19 16:28:59 +0800

    Update install-centos.sh

--- install-centos.sh
+++ install-centos.sh
@@ -10,6 +10,10 @@ git clone https://github.com/FRiCKLE/ngx_cache_purge
 git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
 git clone https://github.com/openresty/headers-more-nginx-module
 git clone https://github.com/yaoweibin/nginx_upstream_check_module
+#install sregex to support replace-filter-nginx-module
+git clone https://github.com/openresty/sregex
+cd sregex
+make && make install
 cd
 #wget https://soft2.vpser.net/lnmp/lnmp1.8.tar.gz -cO lnmp1.8.tar.gz && tar zxf lnmp1.8.tar.gz
 wget https://github.com/tempnana/Lnmp/raw/main/lnmp1.8.tar.gz -cO lnmp1.8.tar.gz && tar zxf lnmp1.8.tar.gz
@@ -24,7 +28,7 @@ sleep 3s
 #./install.sh lnmp
 cd ..
 sed -i 's/soft.vpser.net/soft2.vpser.net/g' lnmp.conf
-sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--with-http_random_index_module --add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge --add-module=/usr/local/headers-more-nginx-module --add-module=/usr/local/nginx_upstream_check_module':" lnmp.conf
+sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--with-http_random_index_module --add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge --add-module=/usr/local/headers-more-nginx-module --add-module=/usr/local/nginx_upstream_check_module --add-module=/usr/local/replace-filter-nginx-module':" lnmp.conf
 chmod +x *.sh
 echo "Choose install:"
 echo ""
