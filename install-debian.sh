48bd6e0036a60ed198204a29cf73934bff80cb1d
tempnana <94152736+tempnana@users.noreply.github.com>
2023-01-19 15:39:12 +0800

    Update install-debian.sh

--- install-debian.sh
+++ install-debian.sh
@@ -5,11 +5,17 @@
 apt update && apt upgrade -y
 ##
 apt-get install curl wget net-tools iftop zip unzip git -y
+#install sregex to support replace-filter-nginx-module
+git clone https://github.com/openresty/sregex
+cd sregex
+make && make install
+#get module files and lnmp source
 cd /usr/local
 git clone https://github.com/FRiCKLE/ngx_cache_purge
 git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
 git clone https://github.com/openresty/headers-more-nginx-module
 git clone https://github.com/yaoweibin/nginx_upstream_check_module
+git clone https://github.com/openresty/replace-filter-nginx-module
 cd
 #wget https://soft2.vpser.net/lnmp/lnmp1.8.tar.gz -cO lnmp1.8.tar.gz && tar zxf lnmp1.8.tar.gz
 wget https://github.com/tempnana/Lnmp/raw/main/lnmp1.8.tar.gz -cO lnmp1.8.tar.gz && tar zxf lnmp1.8.tar.gz
@@ -23,7 +29,7 @@ echo "Install fail2ban..."
 sleep 3s
 cd ..
 sed -i 's/soft.vpser.net/soft2.vpser.net/g' lnmp.conf
-sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--with-http_random_index_module --add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge --add-module=/usr/local/headers-more-nginx-module --add-module=/usr/local/nginx_upstream_check_module':" lnmp.conf
+sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--with-http_random_index_module --add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge --add-module=/usr/local/headers-more-nginx-module --add-module=/usr/local/nginx_upstream_check_module --add-module=/usr/local/replace-filter-nginx-module':" lnmp.conf
 chmod +x *.sh
 echo "Choose install:"
 echo ""
