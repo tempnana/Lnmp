c32f2a5b8f7017a3d76f12362c5388337ac6c4ef
tempnana <94152736+tempnana@users.noreply.github.com>
2023-10-17 14:37:06 +0800

    Update install-centos.sh

--- install-centos.sh
+++ install-centos.sh
@@ -4,7 +4,7 @@
 ###############
 yum update -y && yum upgrade -y
 ##
-yum install curl wget net-tools iftop zip unzip git epel-release -y
+yum install curl wget net-tools iftop zip unzip git epel-release lsof -y
 cd /usr/local
 git clone https://github.com/FRiCKLE/ngx_cache_purge
 git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
