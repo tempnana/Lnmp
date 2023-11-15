b0e8696f6f84f2d9be9b514c5bd5aa7c4f5b5753
tempnana <94152736+tempnana@users.noreply.github.com>
2023-11-15 19:45:15 +0800

    Update add-lua-cjson.sh

--- lualib/add-lua-cjson.sh
+++ lualib/add-lua-cjson.sh
@@ -3,7 +3,7 @@
 #Author: https://github.com/tempnana
 #Source: https://github.com/openresty/lua-cjson
 ###############
-## bash <(wget -qO- https://raw.githubusercontent.com/tempnana/Lnmp/main/install-centos.sh)
+## bash <(wget -qO- https://raw.githubusercontent.com/tempnana/Lnmp/main/lualib/add-lua-cjson.sh)
 ###############
 mkdir /root/lnmp2.0/src-lualib
 cd /root/lnmp2.0/src-lualib
