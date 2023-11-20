45e09ae8b78360ce67b16ca5d09fe523179440af
tempnana <94152736+tempnana@users.noreply.github.com>
2023-11-20 22:05:44 +0800

    Update add-lib-lua-cjson.sh

--- add/add-lib-lua-cjson.sh
+++ add/add-lib-lua-cjson.sh
@@ -4,24 +4,35 @@
 #bash <(wget -qO- https://raw.githubusercontent.com/tempnana/Lnmp/main/add/add-lib-lua-cjson.sh)
 ###############
 
-add_to_CENTOS() {
+install_luarocks_DEBIAN() {
+    default_lua_path='/usr/local/share/lua/5.1'
+    default_lua_cpath='/usr/local/lib/lua/5.1'
+    apt install luarocks -y
+    luarocks install lua-cjson
+    # luarocks show lua-cjson
+    cp ${default_lua_path}/*.lua /usr/local/nginx/lib/lua/
+    cp ${default_lua_cpath}/cjson.so /usr/local/nginx/lib/lua/
+}
+install_luarocks_CENTOS() {
+    default_lua_path='/usr/share/lua/5.1'
+    default_lua_cpath='/usr/lib64/lua/5.1'
     yum install luarocks lua-devel -y
     luarocks install lua-cjson
-
     # luarocks show lua-cjson
-    # Modules:
-    #         cjson (/usr/lib64/lua/5.1/cjson.so)
-    #         cjson.util (/usr/share/lua/5.1/cjson/util.lua)
-    #         json2lua (/usr/share/lua/5.1/json2lua.lua)
-    #         lua2json (/usr/share/lua/5.1/lua2json.lua)
-    # cp -r /usr/share/lua/5.1/* /usr/local/nginx/lib/lua/
+    cp ${default_lua_path}/*.lua /usr/local/nginx/lib/lua/
+    cp ${default_lua_cpath}/cjson.so /usr/local/nginx/lib/lua/
+}
 
-    cp /usr/share/lua/5.1/*.lua /usr/local/nginx/lib/lua/
-    cp /usr/lib64/lua/5.1/cjson.so /usr/local/nginx/lib/lua/
-
-    # # Nginx:
-    # lua_package_cpath "/usr/local/nginx/lib/lua/?.so";
+set_path_to_NGINX() {
     sed -i 's#lua_package_path "/usr/local/nginx/lib/lua/?.lua";#lua_package_path "/usr/local/nginx/lib/lua/?.lua";\nlua_package_cpath "/usr/local/nginx/lib/lua/?.so";#g' /usr/local/nginx/conf/nginx.conf
     lnmp nginx restart
 }
-add_to_CENTOS
+if [ -f /etc/debian_version ]; then
+    install_luarocks_DEBIAN
+    set_path_to_NGINX
+elif [ -f /etc/centos-release ]; then
+    install_luarocks_CENTOS
+    set_path_to_NGINX
+else
+    echo "Unsupported distribution."
+fi
