d90eb673657380f23e684f0646ac7a59b8b5dca3
tempnana <94152736+tempnana@users.noreply.github.com>
2023-11-20 20:04:28 +0800

    Create add-lib-lua-cjson.sh

---
+++ add/add-lib-lua-cjson.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+###############
+#Author: https://github.com/tempnana
+###############
+
+add_to__CENTOS() {
+    yum install luarocks lua-devel -y
+    luarocks install lua-cjson
+
+    # luarocks show lua-cjson
+    # Modules:
+    #         cjson (/usr/lib64/lua/5.1/cjson.so)
+    #         cjson.util (/usr/share/lua/5.1/cjson/util.lua)
+    #         json2lua (/usr/share/lua/5.1/json2lua.lua)
+    #         lua2json (/usr/share/lua/5.1/lua2json.lua)
+    # cp -r /usr/share/lua/5.1/* /usr/local/nginx/lib/lua/
+
+    cp /usr/share/lua/5.1/*.lua /usr/local/nginx/lib/lua/
+    cp /usr/lib64/lua/5.1/cjson.so /usr/local/nginx/lib/lua/
+
+    # # Nginx:
+    # lua_package_cpath "/usr/local/nginx/lib/lua/?.so";
+    sed -i 's#lua_package_path "/usr/local/nginx/lib/lua/?.lua";#lua_package_path "/usr/local/nginx/lib/lua/?.lua";\nlua_package_cpath "/usr/local/nginx/lib/lua/?.so";#g' /usr/local/nginx/conf/nginx.conf
+    lnmp nginx restart
+}
+add_to__CENTOS
